7bfd001f944bbc67563a4a191084e3f8ac614bc5
agung019-UI <dzrgung19@gmail.com>
2024-12-24 02:20:04 +0700

    sudah di layout.tsx tampilan sidebar

--- admin-panel/src/app/admin/layout.tsx
+++ admin-panel/src/app/admin/layout.tsx
@@ -1,10 +1,12 @@
 "use client"
+import Loader from "@/components/admin-apnel/loader";
 import Login from "@/components/admin-apnel/login";
+import Sidebar from "@/components/admin-apnel/sidebar";
 import { useAppSelector } from "@/redux/hook";
 import { useSession } from  "next-auth/react";
 import React from 'react';
 
-const layout = () => {
+const layout = ({children}: {children: React.ReactNode}) => {
     const isLoading = useAppSelector(store => store.loading);
     const {data: session} = useSession();
 
@@ -12,7 +14,14 @@ const layout = () => {
       return <Login/>;
     }
 
-  return <div>layout</div>;
-}
+  return <div className="flex">
+    <Sidebar />
+  <div className="w-full h-full">
+    {/* Navbar/> */} 
+  <div className="bg-gray-200 p-4 h-[cals(100vh-64px)]">{children}</div>
+  </div>
+  {isLoading && <Loader />}
+  </div >
+};
 
 export default layout;
